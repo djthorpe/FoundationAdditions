9ccc815a85aea0f2f320527de845ef2a7500d38c
djthorpe <djt@mutablelogic.com>
2013-02-10 19:55:51 +0000

    added tests

---
+++ tests/NSDateUtilsTests.m
@@ -0,0 +1,12 @@
+
+#import <Foundation/Foundation.h>
+#import "NSDate+Formatting.h"
+#import "NSDate+HTTPUtils.h"
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+
+	}
+    return 0;
+}
+
